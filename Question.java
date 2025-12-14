66f0113a49cbd29dff13ff4b24cfb352c22bbd5b
Danish Fadzil <danishfdzil0503@gmail.com>
2025-12-14 13:55:04 +0800

    Update Question.java

--- Question.java
+++ Question.java
@@ -1,33 +1,25 @@
 public class Question {
+
     private int id;
-    private String questionText;
+    private String text;
     private String correctAnswer;
     private String category; // CS / IS / IT
-    
-    public Question(int id, String questionText, String correctAnswer, String category) {
+
+    public Question(int id, String text, String correctAnswer, String category) {
         this.id = id;
-        this.questionText = questionText;
+        this.text = text;
         this.correctAnswer = correctAnswer;
         this.category = category;
     }
 
-    // Getters and Setters
     public int getId() {
         return id;
     }
 
-    public void setQuestionText(String questionText) {
-        this.questionText = questionText;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
     public String toString() {
-        return "ID: " + id + 
-        "\nQuestion: " + questionText + 
-        "\nCorrect Answer: " + correctAnswer + 
-        "\nCategory: " + category + "\n";
+        return "ID: " + id +
+               " | Question: " + text +
+               " | Answer: " + correctAnswer +
+               " | Category: " + category;
     }
 }
